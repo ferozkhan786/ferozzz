be274acc19a31768447681762e0adf28042f8c35
User <User@192.168.0.9>
2017-01-21 17:02:12 +0530

    hello

--- src/com/pojo/OnetoOneMainClass.java
+++ src/com/pojo/OnetoOneMainClass.java
@@ -11,7 +11,7 @@ public class OnetoOneMainClass {
 	
 	public static void main(String[] args) {
 		
-		 
+		 System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhh");
 		Employee emp=new Employee();
 		emp.setEmpname("Srinivas");
 		emp.setEmpdesignation("Associate");
